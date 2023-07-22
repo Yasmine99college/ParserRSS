e9ffdcf90fb574e2e3b0b7032b01ceb4dc8efd07
Yasmine99college <yasminaahmedabcd@gmail.com>
2023-07-22 13:00:05 +0300

    initial commit

---
+++ BaytRSSParser/src/test/java/com/example/BaytRSSParser/BaytRssParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BaytRSSParser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaytRssParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
